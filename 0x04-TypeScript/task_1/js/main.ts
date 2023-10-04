3b0d4123dbbbe53871636305a5a94d553f689077
marthaakinlolu <marthaakinlolu@gmail.com>
2023-10-04 23:20:41 +0100

    task_1

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,12 @@
+export interface Teacher {
+  readonly firstName: string;
+  readonly lastName: string;
+  fullTimeEmployee: boolean;
+  yearsOfExperience?: number;
+  location: string;
+  [index:string]: any;
+}
+
+export interface Directors extends Teacher {
+  numberOfReports: number;
+}
